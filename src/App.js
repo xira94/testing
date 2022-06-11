f96f8b3e82f2b197417b42d122cff1b57f976bfb
xira94 <guswnschl45@gmail.com>
2022-06-11 23:03:00 +0900

    11:02

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import "./App.css";
-import {  Routes, Route } from "react-router-dom";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Detail from "./Detail";
 import Header from "./Header";
 import Write from "./Write";
-import Signup from "./Signup"
+import Signup from "./Signup";
 import Login from "./Login";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/detail" element={<Detail />} />
-        <Route path='/signup' element={<Signup/>}></Route>
-    <Route path='/login' element={<Login/>}></Route>
-    <Route path='/write' element={<Write/>}></Route>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/write" element={<Write />} />
       </Routes>
     </div>
   );
-
-
+}
 
 export default App;
