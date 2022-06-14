6e2f1fbe41490407a5bda7072860aa6250dc4c1f
김 수정 <sjk5689612@gmail.com>
2022-06-14 20:45:02 +0900

    merge

--- src/Home.js
+++ src/Home.js
@@ -1,24 +1,21 @@
 import React from "react";
 import "./css/Home.css";
-import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
-import { loadPostDB } from "./redux/moduels/post";
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { loadPostDB } from './redux/moduels/post'
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-regular-svg-icons";
 import { faCirclePlus } from "@fortawesome/free-solid-svg-icons";
 
-import { useNavigate } from "react-router-dom";
-
 const Home = () => {
   const dispatch = useDispatch();
 
-  React.useEffect(() => {
+  React.useEffect(()=> {
     dispatch(loadPostDB());
-  }, []);
+  }, [])
 
-  const data = useSelector((state) => state.post.posts);
-  // console.log(data)
+  const data = useSelector(state => state.post.posts)
 
   return (
     <div className="main">
@@ -81,38 +78,34 @@ const Home = () => {
       <div className="board">
         <div className="board_title">
           <h3>Board</h3>
-          <Link to="/write">
-            <div className="btn_post">
-              <FontAwesomeIcon
-                size="2x"
-                color="#186d51"
-                icon={faCirclePlus}
-                onClick={toWrite}
-              />
-            </div>
-          </Link>
+          <Link to ='/write'><div className="btn_post">
+            <FontAwesomeIcon
+              size="2x"
+              color="#186d51"
+              icon={faCirclePlus}
+            />
+          </div></Link>
         </div>
+      <div className="img_collection">
 
-        <div className="img_collection">
-          {data.length !== 0 &&
-            data.map((list, i) => {
-              return (
-                <div className="img_home" key={i}>
-                  {/* <Link to={`write/detail/${data[i].postId}`}> */}
-                  <Link to={`write/detail/${data[i]._id}`}>
-                    <img
-                      alt={data.length !== 0 && data[i].title}
-                      src="https://target.scene7.com/is/image/Target/GUEST_b75d74b7-e711-4bc5-9eb1-d0577622b3e2?wid=488&hei=488&fmt=pjpeg"
-                    ></img>
-                  </Link>
-                  <div className="inner_image">
-                    <p>{data.length !== 0 && data[i].title}</p>
-                    <FontAwesomeIcon size="lg" color="pink" icon={faHeart} />
-                  </div>
-                </div>
-              );
-            })}
+      {data.length !==0 && data.map((list,i)=> {
+        return (
+        <div className="img_home" key={i}>
+          <Link to = {`write/detail/${data[i]._id}`}>
+          <img
+            alt={data.length !==0 && data[i].title}
+            src={data[i].imageUrl}
+          ></img>
+          </Link>
+          <div className="inner_image">
+            <p>{data.length !==0 && data[i].title}</p>
+            <FontAwesomeIcon size="lg" color="pink" icon={faHeart} />
+          </div>
         </div>
+        )
+        })
+      }
+      </div>
       </div>
     </div>
   );
