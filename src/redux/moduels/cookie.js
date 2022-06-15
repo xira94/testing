795382bd850d48388576bb123e7883a033e14934
xira94 <guswnschl45@gmail.com>
2022-06-15 11:36:50 +0900

    220615 11:36

--- src/redux/moduels/cookie.js
+++ src/redux/moduels/cookie.js
@@ -1,3 +1,20 @@
+const getCookie = (name) => {
+  let value = ";" + document.cookie;
+
+  let parts = value.split(`; ${name}=`);
+
+  if (parts.length === 2) {
+    return parts.pop().split(";").shift();
+  }
+};
+
+const setCookie = (name, value, exp = 5) => {
+  let date = new Date();
+  date.setTime(date.getTime() + exp * 24 * 60 * 60 * 1000);
+
+  document.cookie = `${name}=${value}; expires=${date.toUTCString()}`;
+};
+
 const deleteCookie = (name) => {
   let date = new Date("2020-01-01").toUTCString();
   console.log(date);
@@ -5,4 +22,4 @@ const deleteCookie = (name) => {
   document.cookie = name + "=; expires=" + date;
 };
 
-export { deleteCookie };
+export { setCookie, getCookie, deleteCookie };
--- src/redux/moduels/user.js
+++ src/redux/moduels/user.js
@@ -2,11 +2,9 @@
 import axios from "axios";
 
 // Actions
-
 // const ACCOUNT = "user/ACCOUNT";
 const LOGIN = "user/LOGIN";
 const LOGOUT = "user/LOGOUT";
-const LOGIN_CHECK = "user/LOGIN_CHECK";
 
 const initialState = {
   user: null,
@@ -14,27 +12,23 @@ const initialState = {
 };
 
 // Action Creators
+
 export function logInUser(user) {
   return { type: LOGIN, user };
 }
 export function logOutUser(user) {
   return { type: LOGOUT, user };
 }
-export function logincheck(userId, nickname) {
-  return { type: LOGIN_CHECK, userId, nickname };
-}
 
-//middlewares;
-export const signupDB = (userId, nickname, password, passwordCheck) => {
-  console.log(userId, nickname, password, passwordCheck);
+// middlewares
+
+export const signupDB = (userId, nickname, password) => {
   return async function (dispatch, getState) {
-    //naigate 써주고, 이게 window.location.assign이고 난 필요없고.
     await axios
       .post("sparta-swan.shop/api/register", {
         userId: userId,
-        nickname: nickname,
+        nickName: nickname,
         password: password,
-        passwordCheck: passwordCheck,
       })
       .then((user) => {
         console.log(user);
@@ -44,7 +38,6 @@ export const signupDB = (userId, nickname, password, passwordCheck) => {
       .catch((error) => {
         const errorCode = error.code;
         const errorMessage = error.message;
-        console.log("최현준");
         window.alert("회원가입에 실패했습니다! 다시 시도해주세요");
         console.log(errorCode, errorMessage);
       });
@@ -54,88 +47,47 @@ export const signupDB = (userId, nickname, password, passwordCheck) => {
 export const loginDB = (userId, password) => {
   return async function (dispatch) {
     await axios
-      .post("", {
+      .post("sparta-swan.shop/api/login", {
         userId: userId,
         password: password,
       })
       .then((user) => {
         console.log(userId);
         localStorage.setItem("token", user.data.token);
-        dispatch(logInUser());
-        dispatch(logincheckDB());
+        dispatch(
+          logInUser({
+            userId: userId,
+          })
+        );
         window.alert("환영합니다!");
         window.location.assign("/");
       })
       .catch((error) => {
         const errorCode = error.code;
         const errorMessage = error.message;
-        window.alert("로그인에 실패했습니다! 다시 시도해주세요요");
+        window.alert("로그인에 실패했습니다! 다시 시도해주세요");
         console.log(errorCode, errorMessage);
       });
   };
 };
 
 export const logincheckDB = () => {
-  return async function (dispatch) {
-    const _logincheck = await axios
-      .get("")
-      .then((response) => {
-        console.log(response);
-
-        localStorage.setItem("loginUserId", response.data.userId);
-        localStorage.setItem("loginUserName", response.data.nickname);
-
-        dispatch(LOGIN_CHECK(response.data.userId, response.data.nickname));
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+  return function (dispatch) {
+    const userId = localStorage.getItem("userId");
+    const tokenCheck = document.cookie;
+    if (tokenCheck) {
+      dispatch(logInUser({ userId: userId }));
+    } else {
+      dispatch(logOutUser());
+    }
   };
 };
 
-// export const idCheckFB = (userId) => {
-//   console.log(userId);
-//   return async function () {
-//     const _idCheck = await instance
-//       .get(`/api/user//${userId}`)
-//       .then((response) => {
-//         console.log(response);
-//         const message = response.data.message;
-//         window.alert(message);
-//       })
-//       .catch((error) => {
-//         console.error(error);
-//         const error_message = error.response.data.errorMessage;
-//         window.alert(error_message);
-//       });
-//   };
-// };
-
-// export const nicknameCheckFB = (nickname) => {
-//   console.log(nickname);
-//   return async function () {
-//     const _nicknameCheck = await instance
-//       .get(`/api/user//${nickname}`)
-//       .then((response) => {
-//         console.log(response);
-
-//         const message = response.data.message;
-//         window.alert(message);
-//       })
-//       .catch((error) => {
-//         console.error(error);
-//         const error_message = error.response.data.errorMessage;
-//         window.alert(error_message);
-//       });
-//   };
-// };
-
 export const logoutDB = () => {
   return function (dispatch) {
-    localStorage.removeItem("token");
-    localStorage.removeItem("userId");
-    localStorage.removeItem("is_login");
+    localStorage.clear();
     dispatch(logOutUser());
+    window.location.assign("/");
   };
 };
 
@@ -144,14 +96,13 @@ export default function reducer(state = initialState, action = {}) {
   switch (action.type) {
     case LOGIN:
       state.user = { ...action.user };
+      console.log(state.user);
       state.is_login = true;
       return state;
     case LOGOUT:
       state.user = {};
       state.is_login = false;
       return state;
-    case LOGIN_CHECK:
-      return { userId: action.userId, nickname: action.nickname };
     default:
       return state;
   }
