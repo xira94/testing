35b37487805f0369a75d95e0bc41b4d2f0b4c353
xira94 <guswnschl45@gmail.com>
2022-06-16 10:38:13 +0900

    CSS

--- src/commnetWrite.js
+++ src/commnetWrite.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { loadCommentFB, addCommentFB  } from "./redux/moduels/comment";
-import {getPostListDB} from './redux/moduels/post'
+import { loadCommentFB, addCommentFB } from "./redux/moduels/comment";
+import { getPostListDB } from "./redux/moduels/post";
 import "./css/Detail.css";
 
 const Comment = ({ postId }) => {
@@ -11,47 +11,46 @@ const Comment = ({ postId }) => {
     dispatch(loadCommentFB(postId));
   }, []);
   React.useEffect(() => {
-
     dispatch(getPostListDB());
-
   }, []);
 
   const [content, setComment] = React.useState("");
-  console.log(content)
+  console.log(content);
   const data = useSelector((state) => state.comment.comments);
-  const post = useSelector((state)=> state.post.posts);
-  console.log(data)
-  console.log(post)
+  const post = useSelector((state) => state.post.posts);
+  console.log(data);
+  console.log(post);
 
   const onCommentHandler = (e) => {
     setComment(e.currentTarget.value);
   };
 
- 
   const addcomment = () => {
     dispatch(addCommentFB(postId, content));
   };
 
   return (
     <div>
-          <div>
-            <section>
-              <div className="row">
-                <div className="col-sm-5 col-md-6 col12 pb-4">
-                <h2>Comments</h2>
-                {data && data.map((list, i) => {
-                return (
-                  <div className="text-justify darker mt-4 float-right" key={i}>
-                    <h4>{data[i].nickname}</h4>
-                    <p>{data[i].content}</p>
-                  </div>
-                 ); })}
-                </div>
-              </div>
-            </section>
+      <div>
+        <section>
+          <div className="row">
+            <div className="col-sm-5 col-md-6 col12 pb-4">
+              <h2>Comments</h2>
+              {data &&
+                data.map((list, i) => {
+                  return (
+                    <div className="text-justify darker mt-4" key={i}>
+                      <h4>{data[i].nickname}</h4>
+
+                      <p>{data[i].content}</p>
+                    </div>
+                  );
+                })}
+            </div>
           </div>
-        
-      
+        </section>
+      </div>
+
       <section className="data-search">
         <div className="input-group">
           <input
